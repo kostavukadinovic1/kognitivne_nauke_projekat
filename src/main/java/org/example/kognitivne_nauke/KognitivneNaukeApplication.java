309ef1e27092fa54ba48d5492208fec0ca7d6075
VukadinovicLuka <lvukadinovic2921rn@raf.rs>
2026-05-21 03:53:47 +0200

    pocetak projekta

---
+++ src/main/java/org/example/kognitivne_nauke/KognitivneNaukeApplication.java
@@ -0,0 +1,13 @@
+package org.example.kognitivne_nauke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KognitivneNaukeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KognitivneNaukeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kognitivne_nauke/KognitivneNaukeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kognitivne_nauke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KognitivneNaukeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
